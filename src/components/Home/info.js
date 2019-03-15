89b5d4dcb5c6f4a6a584629be8636509741fbbd5
daccatime <dawsonmcnally@outlook.com>
2019-03-15 17:41:22 +0000

    change for background

--- src/components/Home/info.js
+++ src/components/Home/info.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
+import Title from "../Globals/Title"
 
 export default function info() {
   return (
     <section className="py-5">
       <div className="container">
+        <Title title="our story"> </Title>
         <div className="row">
           <div className=" .col-10.col-sm-8 mx-auto text-center">
             <p className="lead text-muted mb-5">
@@ -23,6 +25,11 @@ export default function info() {
               voluptate velit esse quam nihil molestiae consequatur, vel illum
               qui dolorem eum fugiat quo voluptas nulla pariatur?"
             </p>
+            <Link to="/about/">
+              <button className="btn text-uppercase btn-yellow">
+                about page
+              </button>
+            </Link>
           </div>
         </div>
       </div>
