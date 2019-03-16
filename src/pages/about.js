30b44ca4870bf41c33a0ac390f59ec61a7490103
daccatime <dawsonmcnally@outlook.com>
2019-03-16 23:04:29 +0000

    added contenful environment variables and john

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 
-import Info from "../components/Home/info"
+import Info from "../components/Home/Info"
 
 const AboutPage = ({ data }) => (
   <Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,26 +4,44 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
-
-import Info from "../components/Home/info"
-
+import Info from "../components/Home/Info"
+import Menu from "../components/Home/Menu"
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <BackgroundSection
       img={data.img.childImageSharp.fluid}
-      title="Regular Johns"
-      styleClass="default-background"
+      title="regular joe's"
     />
     <Info />
+    <Menu items={data.menu} />
   </Layout>
 )
+
 export const query = graphql`
   {
     img: file(relativePath: { eq: "default-background.jpeg" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          Title
+          description {
+            description
+          }
+          price
+          catagory
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_withWebp_noBase64
+            }
+          }
         }
       }
     }
