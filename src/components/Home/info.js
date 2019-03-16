d35cba4a9bb742ba4c74ff12d9c9054f762c5610
daccatime <dawsonmcnally@outlook.com>
2019-03-16 23:44:22 +0000

    added contenful environment variables 7th time

--- src/components/Home/info.js
+++ src/components/Home/info.js
@@ -1,29 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 import Title from "../Globals/Title"
-
 export default function Info() {
   return (
     <section className="py-5">
       <div className="container">
-        <Title title="our story"> </Title>
+        <Title title="our story" />
         <div className="row">
           <div className="col-10 col-sm-8 mx-auto text-center">
             <p className="lead text-muted mb-5">
-              lorem100 "Sbed ut perspiciatis unde omnis iste natus error sit
-              voluptatem accusantium doloremque laudantium, totam rem aperiam,
-              eaque ipsa quae ab illo inventore veritatis et quasi architecto
-              beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-              voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
-              magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
-              porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-              consectetur, adipisci velit, sed quia non numquam eius modi
-              tempora incidunt ut labore et dolore magnam aliquam quaerat
-              voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem
-              ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-              consequatur? Quis autem vel eum iure reprehenderit qui in ea
-              voluptate velit esse quam nihil molestiae consequatur, vel illum
-              qui dolorem eum fugiat quo voluptas nulla pariatur?"
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sed,
+              ea error similique autem esse nesciunt explicabo quaerat quam,
+              doloremque possimus tenetur excepturi iusto laudantium tempora
+              voluptatum ipsam numquam consequatur vel blanditiis soluta velit
+              necessitatibus nemo? Dignissimos quis iure accusamus beatae modi,
+              autem expedita mollitia enim nobis laborum eos voluptatum.
             </p>
             <Link to="/about/">
               <button className="btn text-uppercase btn-yellow">
