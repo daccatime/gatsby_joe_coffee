e12ee2a8473f66b23d6576daffdf5d933dfd5893
daccatime <dawsonmcnally@outlook.com>
2019-03-16 23:13:51 +0000

    added contenful environment variables 2nd time

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = ({ data }) => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <BackgroundSection
       img={data.img.childImageSharp.fluid}
-      title="regular joe's"
+      title="Regular john's"
     />
     <Info />
     <Menu items={data.menu} />
