4fbc550947dc564a83acc45737834048fb4e3749
daccatime <dawsonmcnally@outlook.com>
2019-03-16 23:58:50 +0000

    added contenful environment variables 8th time

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
-import Info from "../components/Home/Info"
+import Info from "../components/Home/Infom"
 import Menu from "../components/Home/Menu"
 const IndexPage = ({ data }) => (
   <Layout>
