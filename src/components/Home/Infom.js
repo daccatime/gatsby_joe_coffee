a74d8e2799928f2b4b4107f54798e2bc9f6aea58
daccatime <dawsonmcnally@outlook.com>
2019-03-17 00:04:38 +0000

    added contenful environment variables 9th time

--- src/components/Home/Infom.js
+++ src/components/Home/Infom.js
@@ -1,16 +1,29 @@
 import React from "react"
-
-export default function Title({ title }) {
+import { Link } from "gatsby"
+import Title from "../Globals/Title"
+export default function Info() {
   return (
-    <div className="row">
-      <div className="col text-center mb-3">
-        <h1
-          className="display-4  text-capitalize
-              font-weight-bold"
-        >
-          {title}
-        </h1>
+    <section className="py-5">
+      <div className="container">
+        <Title title="our story" />
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Lorem 1ipsum dolor sit amet consectetur adipisicing elit. Ex sed,
+              ea error similique autem esse nesciunt explicabo quaerat quam,
+              doloremque possimus tenetur excepturi iusto laudantium tempora
+              voluptatum ipsam numquam consequatur vel blanditiis soluta velit
+              necessitatibus nemo? Dignissimos quis iure accusamus beatae modi,
+              autem expedita mollitia enim nobis laborum eos voluptatum.
+            </p>
+            <Link to="/about/">
+              <button className="btn text-uppercase btn-yellow">
+                about page
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
